427ca2da241c4a5a2e0918d496cfd52b5534a136
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-05-21 17:30:47 +0700

    #0 Init project

---
+++ script.js
@@ -0,0 +1,38 @@
+const songList = [
+  {
+    name: "Some Thing Just Like This",
+    artist: "The Chainsmokers & Coldplay",
+    src: "assets/songs/something-just-like-this.mp3",
+    cover: "assets/covers/something-just-like-this.png"
+  },
+  {
+    name: "There's Nothing Holdin' Me Back",
+    artist: "Shawn Mendes",
+    src: "assets/songs/there-nothing-holding-me-back.mp3",
+    cover: "assets/covers/there-nothing-holding-me-back.png"
+  },
+  {
+    name: "Summertime",
+    artist: "K-391",
+    src: "assets/songs/summertime.mp3",
+    cover: "assets/covers/summertime.png"
+  },
+  {
+    name: "Reality",
+    artist: "Lost Frequencies",
+    src: "assets/songs/reality.mp3",
+    cover: "assets/covers/reality.png"
+  },
+  {
+    name: "That's Girl",
+    artist: "Olly Murs",
+    src: "assets/songs/that-girl.mp3",
+    cover: "assets/covers/that-girl.png"
+  },
+  {
+    name: "Shape Of You",
+    artist: "Ed Sheeran",
+    src: "assets/songs/shape-of-you.mp3",
+    cover: "assets/covers/shape-of-you.png"
+  }
+]
